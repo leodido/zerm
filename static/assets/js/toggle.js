61c3fe38f2ce3201b171e33b6f9a10dbe91981fe
Leonardo Di Donato <leodidonato@gmail.com>
2021-05-27 17:06:59 +0200

    new: persist the current color theme via local storage
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- static/assets/js/toggle.js
+++ static/assets/js/toggle.js
@@ -7,16 +7,19 @@ function isMobile() {
     return window.matchMedia(c).matches
 }
 
-function toggleClass(nodeEl, className) {
-    let hasClass = nodeEl.classList.contains(className);
+function toggleClass(nodeEl, className, hasClass) {
     theme.disabled = !hasClass;
     nodeEl.classList.toggle(className, !hasClass);
+    localStorage.setItem("theme-storage", hasClass ? "light" : "default");
 }
 
 theme && toggles && toggles.forEach((toggle) => {
     toggle.addEventListener("click", (e) => {
         e.preventDefault();
         menu && menu.classList.toggle("hidden", isMobile());
-        toggles.forEach((t) => toggleClass(t, "default-color"));
+        toggles.forEach((t) => toggleClass(t, "default-color", t.classList.contains("default-color")));
     })
 });
+
+var save = localStorage.getItem("theme-storage") || "default";
+toggles.forEach((t) => toggleClass(t, "default-color", save !== "default"));
